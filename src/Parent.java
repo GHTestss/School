c2803951f4a16ff6b09990f5b35140f0e8ef2981
TetianaLutsenko <tetiana.lutsenko.36@gmail.com>
2021-07-26 20:47:21 +0200

    Abstract classes and methods

---
+++ src/Parent.java
@@ -0,0 +1,14 @@
+public final class Parent extends BasePerson {
+
+
+    Parent(String firstName, String lastName, char gender, int age, String status) {
+        super(firstName, lastName, gender, age, status);
+    }
+
+    @Override
+    public void setAge(int age) {
+        if (age > 18) {
+            this.setAge(age);
+        }
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,28 @@
+public final class Student extends BasePerson {
+
+    private final int grade;
+
+    Student (String firstName, String lastName, char gender, int age, String status, int grade) {
+        super(firstName, lastName, gender, age, status);
+        this.grade = grade;
+    }
+
+    @Override
+    public void setAge(int age) {
+        if (age > 10) {
+            this.setAge(age);
+        }
+    }
+
+    @Override
+    public void setGender(char gender) {
+        if (gender == 'M' || gender == 'F') {
+            super.setGender(gender);
+        }
+        else {
+            System.out.println("Error");
+        }
+    }
+
+
+}
